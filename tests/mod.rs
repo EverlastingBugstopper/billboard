d7b2c9270530bf01c284d18ee991ba60b433b74d
Phil Prasek <prasek@gmail.com>
2021-05-28 10:54:35 -0700

    add eprint test
    Signed-off-by: Phil Prasek <prasek@gmail.com>

--- tests/mod.rs
+++ tests/mod.rs
@@ -1,3 +1,6 @@
+use assert_cmd::Command;
+use predicates::prelude::*;
+
 #[test]
 fn test_readme_deps() {
     version_sync::assert_markdown_deps_updated!("README.md");
@@ -6,4 +9,13 @@ fn test_readme_deps() {
 #[test]
 fn test_html_root_url() {
     version_sync::assert_html_root_url_updated!("src/lib.rs");
+}
+
+#[test]
+fn test_eprint() {
+    let mut cmd = Command::cargo_bin("examples/hello_world").unwrap();
+
+    // running the CLI with no command returns to std err
+    let result = cmd.assert();
+    result.stderr(predicate::str::contains("Hello, World!"));
 }
